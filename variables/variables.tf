c2f7157bc614d5bd5283e7d5b8cd323768f5d46e
Arpitha <Shettyarpithasg@gmail.com>
2026-03-11 16:18:21 +0530

    added new files

---
+++ variables/variables.tf
@@ -0,0 +1,48 @@
+variable "ec2_ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "AMI ID for EC2 instance"
+}
+
+variable "ec2_instance_type" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map(string)
+
+  default = {
+    Name        = "backend"
+    Project     = "expense"
+    Component   = "backend"
+    Environment = "DEV"
+    Terraform   = "true"
+  }
+}
+
+variable "security_group_name" {
+  default = "terraform-ssh-sg"
+}
+
+variable "security_group_description" {
+  default = "Allow SSH access on port 22"
+}
+
+variable "ssh_from_port" {
+  type    = number
+  default = 22
+}
+
+variable "ssh_to_port" {
+  type    = number
+  default = 22
+}
+
+variable "ssh_protocol" {
+  default = "tcp"
+}
+
+variable "allowed_cidr" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
