806205cb68c518c8f136818d721d7a6102f103cf
Arpitha <Shettyarpithasg@gmail.com>
2026-03-21 18:11:07 +0530

    added new files

--- terraformpractice/variables/ec2.tf
+++ terraformpractice/variables/ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "terraform" {
 
-    ami = "var.ami_id"
-    instance_type = "var.instance_type"
+    ami = var.ami_id
+    instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = var.tags
 }  
 
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name        = "var.sg_name"
-    description = "var.sg_description" #port number 22 
+    name        = var.sg_name
+    description = var.sg_description #port number 22 
 
     # usually we allow everything in egress
     egress {
