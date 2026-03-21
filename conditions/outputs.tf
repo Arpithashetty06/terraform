c227264fafb6719373d91a9858503c9942d9e311
Arpitha <Shettyarpithasg@gmail.com>
2026-03-21 23:37:10 +0530

    added new files

--- conditions/outputs.tf
+++ conditions/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_id" {
-value = aws_instance.terraform_server.id
+    value = aws_instance.terraform_server.id
 }
+
+output "public_ip" {
+    value = aws_instance.terraform_server.public_ip
+  }
