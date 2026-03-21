67e4a0dc0bf22fa77da3a53c91b7f00afc5a2ef6
Arpitha <Shettyarpithasg@gmail.com>
2026-03-21 16:56:42 +0530

    added new files

--- terraformpractice/variables/ec2.tf
+++ terraformpractice/variables/ec2.tf
@@ -1,15 +1,10 @@
 resource "aws_instance" "terraform" {
+
     ami = "var.ami_id"
     instance_type = "var.instance_type"
-
-    vpc_security_group_ids = [
-    aws_security_group.allow_ssh_terraform.id
-  ]
-
-  tags = {
-    Name = "tf-ec2-instance"
-  }
-}
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = var.tags
+}  
 
 
 resource "aws_security_group" "allow_ssh_terraform" {
