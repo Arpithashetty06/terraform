a8acb48480d027a0b94a4263abcea85f5c16d07e
Arpitha <Shettyarpithasg@gmail.com>
2026-03-21 10:55:19 +0530

    added new files

---
+++ terraformpractice/variables/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-051a31ab2f4d498f5"
+    instance_type = "t3.micro"
+
+    vpc_security_group_ids = [
+    aws_security_group.allow_ssh_terraform.id
+  ]
+
+  tags = {
+    Name = "tf-ec2-instance"
+  }
+}
+
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "allow_ssh_sg" 
+    description = "Allow SSH access" #port number 22
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1" #-1 means allow all outbound traffic
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = 22
+        to_port          = 22 #22 is default ssh port
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone or allow anywhere on the internet
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    tags = {
+        Name = "tf-allow-ssh-sg"
+    }
+}    
