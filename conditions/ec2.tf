d153fa30d8351046c57e70d6a44804ccb529a998
Arpitha <Shettyarpithasg@gmail.com>
2026-03-21 22:58:22 +0530

    added new files

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform" {
 
   ami = "ami-02dfbd4ff395f2a1b"
-  instance_type = "t3.micro"
+  instance_type = var.environment == "prod" ? "t3.micro" : "t3.small"
   vpc_security_group_ids = [aws_security_group.allow_sg_ssh.id]
 
   tags = {
