4c3c37ff0cecd9648e99006b483aacc51ad30931
Arpitha <Shettyarpithasg@gmail.com>
2026-03-22 15:29:04 +0530

    added new files

--- expense/ec2.tf
+++ expense/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "expense" {
     count = length(var.instance_names)
-    ami = "ami-09c813fb71547fc4f"
+    ami = "ami-02dfbd4ff395f2a1b"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     # tags = {
