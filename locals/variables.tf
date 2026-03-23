79bc1e6b4d32b5164345067aee720697eeead7bc
Arpitha <Shettyarpithasg@gmail.com>
2026-03-23 21:02:27 +0530

    added new files

---
+++ locals/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "zone_id" {
+  default = "Z01697291CA4WUNGQJD8J"
+}
+
+variable "domain_name" {
+  default = "5starshetty.com"
+}
