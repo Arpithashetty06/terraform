294cb141a365ca4b7adcb5d7f4de22587062ca75
Arpitha <Shettyarpithasg@gmail.com>
2026-03-24 02:14:00 +0530

    added new files

---
+++ remotestate/aws_ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-051a31ab2f4d498f5"
+    instance_type = "t3.micro"
+
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+  tags = {
+    Name = "tf-ec2-instance"
+  }
+}
---
+++ remotestate/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.35.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "shetty-remote-state"
+    key            = "remote-state-demo"
+    region         = "us-east-1"
+    dynamodb_table = "5starshetty-locking"
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
