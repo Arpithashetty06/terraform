1699c6f4d71289f97300ba72ec877d51d44c5b2f
Arpitha <Shettyarpithasg@gmail.com>
2026-03-24 02:20:00 +0530

    added new files

--- remotestate/aws_ec2.tf
+++ remotestate/aws_ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-    ami = "ami-051a31ab2f4d498f5"
+    ami = "ami-02dfbd4ff395f2a1b"
     instance_type = "t3.micro"
 
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
