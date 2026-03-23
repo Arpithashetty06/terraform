507a7ca4ff2528ed86db5d4dec8e017127374744
Arpitha <Shettyarpithasg@gmail.com>
2026-03-23 14:52:45 +0530

    added new files

--- expense/data.tf
+++ expense/data.tf
@@ -1,19 +1,14 @@
 data "aws_ami" "ami_info" {
-    most_recent = true
-    owners = ["973714476881"]
+  most_recent = true
+  owners      = ["amazon"]
 
-    filter {
-      name = "name"
-      values = ["RHEL-9-DevOps-Practice"]
-    }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
 
-    filter {
-      name = "root-device-type"
-      values = ["ebs"]
-    }
-
-    filter {
-      name = "virtualization-type"
-      values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
