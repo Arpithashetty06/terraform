0773247e28086d14008c79917d8386bea4a65613
Arpitha <Shettyarpithasg@gmail.com>
2026-03-24 23:16:59 +0530

    added new files

--- for-each/aws_ec2.tf
+++ for-each/aws_ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "terraform" {
-    ami = "ami-02dfbd4ff395f2a1b"
-    for_each = var.instances
-    instance_type = "t3.micro"
+  ami           = "ami-02dfbd4ff395f2a1b"
+  for_each      = var.instances
+  instance_type = "t3.micro"
 
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
 
   tags = {
     Name = "tf-ec2-instance"
