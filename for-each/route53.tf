68fc06380b9714d477ffbcf1b1c04918208ce19b
Arpitha <Shettyarpithasg@gmail.com>
2026-03-25 14:15:13 +0530

    added new files

--- for-each/route53.tf
+++ for-each/route53.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_record" "terraform" {
   zone_id = var.zone_id
+  for_each = aws_instance.terraform
   #backend.daws81s.online
   name            = each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
   type            = "A"
