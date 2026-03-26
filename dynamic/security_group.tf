d6b42608aeb2a58b21e41ccdbd6d7d1137020492
Arpitha <Shettyarpithasg@gmail.com>
2026-03-26 21:46:57 +0530

    added new files

--- dynamic/security_group.tf
+++ dynamic/security_group.tf
@@ -13,12 +13,12 @@ resource "aws_security_group" "allow_ssh_terraform" {
 
     dynamic "ingress" {
         for_each = var.ingress_rules
-        
+
         content {
-        from_port        = ingress.value.from_port
-        to_port          = ingress.value.to_port
-        protocol         = ingress.value.protocol
-        cidr_blocks      = ingress.value.cidr_block
+            from_port        = ingress.value.from_port
+            to_port          = ingress.value.to_port
+            protocol         = ingress.value.protocol
+            cidr_blocks      = ingress.value.cidr_blocks
         }
     }
 
