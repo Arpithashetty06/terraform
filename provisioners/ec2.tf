889a750322d8660f170107e129fcd22baefa5ede
Arpitha <Shettyarpithasg@gmail.com>
2026-03-27 13:19:08 +0530

    added new files

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -1,17 +1,35 @@
-resource "aws_instance" "terraform_p" {
+resource "aws_instance" "terraform" {
   count = 3
   ami = "ami-02dfbd4ff395f2a1b"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_sg_ssh.id]
 
   tags = {
-    Name = "terraform_p"
+    Name = "terraform"
    }
 
    provisioner "local-exec" {
       command = "echo ${self.private_ip} > public_ip.txt"
    }
+
+   connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = self.public_ip
+   }
+
+   provisioner "remote-exec" {
+      inline = [ 
+         "sudo dnf install ansible -y",
+         "sudo dnf install nginx -y",
+         "sudo systemctl start nginx",
+      ]
+     
+   }
+
 }
+
 
 resource "aws_security_group" "allow_sg_ssh" {
     description = "allowing port number 22 ssh"
@@ -30,6 +48,14 @@ resource "aws_security_group" "allow_sg_ssh" {
       ingress {
         from_port        = 22
         to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+      }
+
+      ingress {
+        from_port        = 80
+        to_port          = 80
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
